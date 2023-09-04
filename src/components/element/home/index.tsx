0ba46e1cbaf13daee6b4e826ca1b2079fbacf16a
lulupang2 <lulupang@live.co.kr>
2023-09-04 22:42:50 +0900

    ♻️ refactor: 사용하지 않는 라이브러리 제거

--- src/components/element/home/index.tsx
+++ src/components/element/home/index.tsx
@@ -1,10 +1,8 @@
 "use client";
-import React from "react";
-import clsx from "clsx";
 import { HeroImage } from "@generated/images/myImages";
 import { classes } from "@libs/classes";
-import { useTranslations } from "next-intl";
 import { motion } from "framer-motion";
+import { useTranslations } from "next-intl";
 
 const cn = (str: string) => classes(`home-` + str);
 
